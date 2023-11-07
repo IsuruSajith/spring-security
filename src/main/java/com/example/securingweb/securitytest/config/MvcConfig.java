ad6f260b4f3bcdf7ac5cfa887ae418481907a376
Isuru <isurusajith0@gmail.com>
2023-11-07 12:00:00 +0530

    Committing changes from the past few days

---
+++ src/main/java/com/example/securingweb/securitytest/config/MvcConfig.java
@@ -0,0 +1,7 @@
+package com.example.securingweb.securitytest.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+}

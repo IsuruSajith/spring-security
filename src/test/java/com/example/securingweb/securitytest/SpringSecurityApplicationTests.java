8bf5ccd97d9df88abea47dafe17ccee7d3218bf5
Isuru <isurusajith0@gmail.com>
2023-11-19 09:07:20 +0530

    add files to github

---
+++ src/test/java/com/example/securingweb/securitytest/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securingweb.securitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

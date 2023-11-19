14ff474009d7138d0e2b31723fa0acc45ccd838b
Isuru <isurusajith0@gmail.com>
2023-11-19 14:29:07 +0530

    implement user details

--- src/main/java/com/example/securingweb/securitytest/entity/User.java
+++ src/main/java/com/example/securingweb/securitytest/entity/User.java
@@ -1,13 +1,16 @@
 package com.example.securingweb.securitytest.entity;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Data
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -17,4 +20,33 @@ public class User {
     private String password;
     private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }

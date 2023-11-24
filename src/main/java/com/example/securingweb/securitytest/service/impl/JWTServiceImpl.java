68e804d3b8338d02addacce4d889c9dd2f8c7b12
Isuru <isurusajith0@gmail.com>
2023-11-24 09:54:23 +0530

    going to generate a key from private method

--- src/main/java/com/example/securingweb/securitytest/service/impl/JWTServiceImpl.java
+++ src/main/java/com/example/securingweb/securitytest/service/impl/JWTServiceImpl.java
@@ -1,5 +1,32 @@
 package com.example.securingweb.securitytest.service.impl;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
 public class JWTServiceImpl {
+
+    private String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private Key getSignKey() {
+
+        byte[] key = Decoders.BASE64.decode("");
+
+        return Keys.hmacShaKeyFor(key);
+    }
 
 }

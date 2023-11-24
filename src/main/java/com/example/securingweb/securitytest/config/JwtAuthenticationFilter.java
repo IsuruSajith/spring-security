9871050a608159d18c26d19f5c7883d0edc5c37c
Isuru <isurusajith0@gmail.com>
2023-11-24 11:07:37 +0530

    <!-- https://mvnrepository.com/artifact/org.apache.commons/commons-lang3 --> 		<dependency> 			<groupId>org.apache.commons</groupId> 			<artifactId>commons-lang3</artifactId> 			<version>3.12.0</version> 		</dependency>

--- src/main/java/com/example/securingweb/securitytest/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/securingweb/securitytest/config/JwtAuthenticationFilter.java
@@ -1,5 +1,9 @@
 package com.example.securingweb.securitytest.config;
 
+import com.example.securingweb.securitytest.service.JWTService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,10 +11,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.apache.commons.lang3.StringUtils;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+    private final UserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if(StringUtils.isEmpty(authHeader)|| !org.apache.commons.lang3.StringUtils.startsWith(authHeader,"Bearer "))
+
 
     }
 }

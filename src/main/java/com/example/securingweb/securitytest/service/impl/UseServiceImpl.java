e3a7453d45ebc060f51f38762010971ef5c212ff
Isuru <isurusajith0@gmail.com>
2023-11-24 11:20:12 +0530

    override two methods

--- src/main/java/com/example/securingweb/securitytest/service/impl/UseServiceImpl.java
+++ src/main/java/com/example/securingweb/securitytest/service/impl/UseServiceImpl.java
@@ -1,10 +1,29 @@
 package com.example.securingweb.securitytest.service.impl;
 
+import com.example.securingweb.securitytest.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UseServiceImpl {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        }
+
+    }
 
 }

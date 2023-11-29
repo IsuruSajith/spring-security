b69c3c464a37736319b67e2a972bfb27e1634205
Isuru <isurusajith0@gmail.com>
2023-11-29 12:03:31 +0530

    create security

--- src/main/java/com/example/securingweb/securitytest/config/SecurityConfig.java
+++ src/main/java/com/example/securingweb/securitytest/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.example.securingweb.securitytest.config;
 
 import com.example.securingweb.securitytest.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +17,12 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
 
-
+@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request ->request.regexMatchers("/api/v1/auth/**")
+                    .permitAll()
+                    .requestMatchers())
+}
 
 }

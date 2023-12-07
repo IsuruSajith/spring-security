e56d6923dff5ab81428d525dc700be63225e54d0
Isuru <isurusajith0@gmail.com>
2023-12-07 12:03:31 +0530

    update security config class

--- src/main/java/com/example/securingweb/securitytest/config/SecurityConfig.java
+++ src/main/java/com/example/securingweb/securitytest/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.securingweb.securitytest.config;
 
+import com.example.securingweb.securitytest.entity.Role;
 import com.example.securingweb.securitytest.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +20,13 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
 
-@Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(request ->request.regexMatchers("/api/v1/auth/**")
-                    .permitAll()
-                    .requestMatchers())
-}
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request -> request.regexMatchers("/api/v1/auth/**").permitAll().requestMatchers("api/v1/admin").hasAnyAuthority(Role.ADMIN.name()).requestMatchers("api/v1/user").hasAnyAuthority(Role.USER.name()).anyRequest().authenticated())
+                .sessionManagement(manager->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(
+                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
+                )
+    }
 
 }

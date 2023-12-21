62ce4dd508966e1f809fccabe1e1120f1df09cb9
Isuru <isurusajith0@gmail.com>
2023-12-21 12:03:31 +0530

    update application.java class

--- src/main/java/com/example/securingweb/securitytest/SpringSecurityApplication.java
+++ src/main/java/com/example/securingweb/securitytest/SpringSecurityApplication.java
@@ -1,13 +1,23 @@
 package com.example.securingweb.securitytest;
 
+import com.example.securingweb.securitytest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringSecurityApplication {
+public class SpringSecurityApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
